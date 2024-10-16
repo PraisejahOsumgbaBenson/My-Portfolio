dda6dd91f1773d5231ab34e6d3fd4688a785170a
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-10-16 11:36:25 +0100

    feat: implement header component and set up app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing Router and Routes for navigation
 import "./Style.css"; // Importing global styles
-import Home from "./components/Home";
-import Cursor from "./custom/cursor";
+import Home from "./components/Home"; // Importing the Home component for the '/' route
+import Cursor from "./components/Cursor"; // Importing the custom Cursor component
 
 function App() {
   return (
     <Router>
       <div className="App">
+        {/* Custom cursor component */}
         <Cursor />
+
         {/* Routing setup with home route */}
         <Routes>
-          <Route path="/" element={<Home />} />{" "}
           {/* Default route set to Home component */}
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
