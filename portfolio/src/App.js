3a25dc6898ae30f28054c73ffcb08873a27aa31e
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-10-16 11:27:15 +0100

    feat: implement header component and set up app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./Style.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing Router and Routes for navigation
+import "./Style.css"; // Importing global styles
 import Home from "./components/Home";
-import Cursor from "./components/cursor";
+import Cursor from "./custom/cursor";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Cursor />
+        {/* Routing setup with home route */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />{" "}
+          {/* Default route set to Home component */}
         </Routes>
       </div>
     </Router>
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -5,36 +5,45 @@ function Header() {
   return (
     <header className="header">
       <div className="header-left">
-        {/* Logo */}
+        {/* Logo section */}
         <p className="logo">PJ</p>
 
-        {/* Box for Home and About links */}
+        {/* Navigation box containing Home and About links */}
         <div className="nav-box">
-          <div className="v-line"></div> {/* Left vertical line */}
+          <div className="v-line"></div>{" "}
+          {/* Left vertical line before HOME link */}
+          {/* Home link */}
           <Link to="/" className="nav-link home">
             HOME
           </Link>
-          <div className="ve-line"></div> {/* Right vertical line */}
+          <div className="ve-line"></div>{" "}
+          {/* Vertical line between HOME and ABOUT links */}
+          {/* About link */}
           <Link to="/about" className="nav-link">
             ABOUT
           </Link>
-          <div className="v-line"></div> {/* Right vertical line */}
+          <div className="v-line"></div>{" "}
+          {/* Right vertical line after ABOUT link */}
         </div>
       </div>
 
+      {/* Centered navigation bar with links to Projects, Skills, and Qualifications */}
       <nav className="header-center">
         <ul className="nav-list">
           <li>
+            {/* Projects link */}
             <Link to="/projects" className="nav-link">
               PROJECT
             </Link>
           </li>
           <li>
+            {/* Skills link */}
             <Link to="/skills" className="nav-link">
               SKILLS
             </Link>
           </li>
           <li>
+            {/* Qualifications link */}
             <Link to="/qualifications" className="nav-link">
               QUALIFICATIONS
             </Link>
@@ -42,13 +51,14 @@ function Header() {
         </ul>
       </nav>
 
+      {/* Right side of the header with Services link */}
       <div className="header-right">
         <Link to="/services" className="nav-link">
           SERVICES
         </Link>
       </div>
 
-      {/* Horizontal line starts after the logo */}
+      {/* Horizontal line extending from the logo to the rest of the header */}
       <div className="h-line"></div>
     </header>
   );
