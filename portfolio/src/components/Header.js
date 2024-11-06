7324e177f28b41588414110148ab5bd4a3bd13b1
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-11-06 19:00:41 +0100

    feat(header):simplify navigations and update links

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -6,60 +6,38 @@ function Header() {
     <header className="header">
       <div className="header-left">
         {/* Logo section */}
-        <p className="logo">PJ</p>
-
-        {/* Navigation box containing Home and About links */}
-        <div className="nav-box">
-          <div className="v-line"></div>{" "}
-          {/* Left vertical line before HOME link */}
-          {/* Home link */}
-          <Link to="/" className="nav-link home">
-            HOME
-          </Link>
-          <div className="ve-line"></div>{" "}
-          {/* Vertical line between HOME and ABOUT links */}
-          {/* About link */}
-          <Link to="/about" className="nav-link">
-            ABOUT
-          </Link>
-          <div className="v-line"></div>{" "}
-          {/* Right vertical line after ABOUT link */}
-        </div>
+        <p className="logo">PJ.</p>
       </div>
 
-      {/* Centered navigation bar with links to Projects, Skills, and Qualifications */}
+      {/* navigation bar*/}
       <nav className="header-center">
         <ul className="nav-list">
+          <li>
+            <Link to="/" className="nav-link home">
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="nav-link">
+              ABOUT
+            </Link>
+          </li>
           <li>
             {/* Projects link */}
             <Link to="/projects" className="nav-link">
               PROJECT
             </Link>
           </li>
+
           <li>
             {/* Skills link */}
             <Link to="/skills" className="nav-link">
-              SKILLS
-            </Link>
-          </li>
-          <li>
-            {/* Qualifications link */}
-            <Link to="/qualifications" className="nav-link">
-              QUALIFICATIONS
+              CONTACT
             </Link>
           </li>
         </ul>
       </nav>
 
-      {/* Right side of the header with Services link */}
-      <div className="header-right">
-        <Link to="/services" className="nav-link">
-          SERVICES
-        </Link>
-      </div>
-
-      {/* Horizontal line extending from the logo to the rest of the header */}
-      <div className="h-line"></div>
     </header>
   );
 }
